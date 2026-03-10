b15c2973f54d83c9ab244b4cd65f6e1ff472bafb
Kinshuk26 <kinshukj26@gmail.com>
2026-03-10 09:11:31 +0530

    Made Changes

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,10 @@
+/* 
+@version 2.0
+@author Kinshuk
+ */
 public class HelloApp{
     public static void main (String[] args){
-        System.out.println("Hello, World!");
+       String name = args[0];
+        System.out.println("Hello " + name + "!");
     }
 }
