b54da52ce290849504154b1ef275132508056a57
Kinshuk26 <kinshukj26@gmail.com>
2026-03-17 08:46:34 +0530

    Made Changes

--- HelloApp.java
+++ HelloApp.java
@@ -1,21 +1,21 @@
 /* 
-@version 4.0
+@version 5.0
 @author Kinshuk
  */
 public class HelloApp{
     public static void main (String[] args){
         StringBuilder nameBuilder = new StringBuilder();
-        for (int i = 0; i < args.length; i++){
-            nameBuilder.append(args[i]);
-            if (i < args.length - 1){
+        boolean first = true;
+        for (String name : args){
+            if (! first){
                 nameBuilder.append(", ");
             }
+            nameBuilder.append(name);
+            first = false;
         }
         if (args.length == 0){
         System.out.println("Hello, World!");
-        return;
-        }
-        String name = nameBuilder.toString();
-        System.out.println("Hello " + name + "!");
+        return;}
+        System.out.println("Hello " + nameBuilder + "!");
     }
 }
